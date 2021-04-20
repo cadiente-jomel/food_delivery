175acf533ebd05da4b5d3a743b3bf30ace8a299a
Jomel Cadiente <cadientejomel0@gmail.com>
2021-04-20 13:54:25 +0800

    added: form

--- food_delivery/user/forms.py
+++ food_delivery/user/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Customer, Profile
 
+from core.models import CustomerShippingAddress
+
 
 class CustomerRegisterForm(UserCreationForm):
     class Meta:
@@ -18,3 +20,8 @@ class CustomerProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+class CustomerShippingAddressForm(forms.ModelForm):
+    class Meta:
+        model = CustomerShippingAddress
+        fields = '__all__'
